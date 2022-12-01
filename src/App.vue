0e2a46176858afb21f0c47f48463e2dc027ffd42
renato-17 <renato.avela.17@gmail.com>
2022-12-01 13:05:30 -0500

    <config>: creating base project configuration

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ToDoList from '@/components/todo-list.component.vue'
+</script>
+
+<template>
+  <ToDoList />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/todo-list.component.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Template</h1>
+</template>
+
+<style lang="scss">
+</style>
+
+<script setup>
+</script>
